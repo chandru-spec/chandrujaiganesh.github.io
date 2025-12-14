7cf1891250b438fe256b9467afcb472591b0aae2
Chandru Jaiganesh <jpjchandru@gmail.com>
2025-12-14 19:05:26 +0530

    Fix paths and add nojekyll

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
+  basePath: "/chandrujaiganesh", // This MUST match your repository name exactly
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
